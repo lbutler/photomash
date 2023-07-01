c4eeaa710366ab6cfef225e44309137bfeabaed7
Luke Butler <lukepbutler@gmail.com>
2023-07-01 14:32:09 -0400

    auto create photo array, use callback

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="title">Photo Ranking App</h1>
+      <h1 className="title">Photo Ranking</h1>
       <PhotoRank />
     </>
   );
--- src/PhotoRank.tsx
+++ src/PhotoRank.tsx
@@ -9,53 +9,29 @@ interface Photo {
   url: string;
 }
 
-const intitalPhotos: Photo[] = [
-  { id: 1, url: "001.jpg", rating: 1000, score: 0 },
-  { id: 2, url: "002.jpg", rating: 1000, score: 0 },
-  { id: 3, url: "003.jpg", rating: 1000, score: 0 },
-  { id: 4, url: "004.jpg", rating: 1000, score: 0 },
-  { id: 5, url: "005.jpg", rating: 1000, score: 0 },
-  { id: 6, url: "006.jpg", rating: 1000, score: 0 },
-  { id: 7, url: "007.jpg", rating: 1000, score: 0 },
-  { id: 8, url: "008.jpg", rating: 1000, score: 0 },
-  { id: 9, url: "009.jpg", rating: 1000, score: 0 },
-  { id: 10, url: "010.jpg", rating: 1000, score: 0 },
-  // Add more photos as needed
-];
+const createPhotosArray = (): Photo[] => {
+  const initialPhotos = [];
+
+  for (let i = 1; i <= 72; i++) {
+    const id = i;
+    const url = `${String(i).padStart(3, "0")}.jpg`;
+    const rating = 1000;
+    const score = 0;
+
+    initialPhotos.push({ id, url, rating, score });
+  }
+
+  return initialPhotos;
+};
 
 const PhotoRankingApp: React.FC = () => {
-  const [photos, setPhotos] = useState<Photo[]>(intitalPhotos);
+  const [photos, setPhotos] = useState<Photo[]>(createPhotosArray());
   const [photoIndex1, setPhotoIndex1] = useState<number>(0);
   const [photoIndex2, setPhotoIndex2] = useState<number>(1);
   const [stabilized, setStabilized] = useState<boolean>(false);
   const [iteration, setIteration] = useState(0);
 
-  const handlePhotoSelection = (selectedPhotoIndex: number) => {
-    const updatedPhotos = [...photos];
-    const photo1 = updatedPhotos[photoIndex1];
-    const photo2 = updatedPhotos[photoIndex2];
-
-    const expectedScore1 =
-      1 / (1 + 10 ** ((photo2.rating - photo1.rating) / 400));
-    const expectedScore2 = 1 - expectedScore1;
-
-    const kFactor = 32; // Adjust this value to control the rate of rating change
-
-    const score1 = selectedPhotoIndex === 0 ? 1 : 0;
-    const score2 = selectedPhotoIndex === 1 ? 1 : 0;
-
-    const newRating1 = photo1.rating + kFactor * (score1 - expectedScore1);
-    const newRating2 = photo2.rating + kFactor * (score2 - expectedScore2);
-
-    photo1.rating = newRating1;
-    photo2.rating = newRating2;
-
-    setIteration(iteration + 1);
-    setPhotos(updatedPhotos);
-    setNextPhotoIndices();
-  };
-
-  const setNextPhotoIndices = () => {
+  const setNextPhotoIndices = useCallback(() => {
     const numPhotos = photos.length;
 
     if (numPhotos <= 2) {
@@ -71,7 +47,43 @@ const PhotoRankingApp: React.FC = () => {
       setPhotoIndex1(newPhotoIndex1);
       setPhotoIndex2(newPhotoIndex2);
     }
-  };
+  }, [photos.length]);
+
+  const handlePhotoSelection = useCallback(
+    (selectedPhotoIndex: number) => {
+      const updatedPhotos = [...photos];
+      const photo1 = updatedPhotos[photoIndex1];
+      const photo2 = updatedPhotos[photoIndex2];
+
+      const expectedScore1 =
+        1 / (1 + 10 ** ((photo2.rating - photo1.rating) / 400));
+      const expectedScore2 = 1 - expectedScore1;
+
+      const kFactor = 32; // Adjust this value to control the rate of rating change
+
+      const score1 = selectedPhotoIndex === 0 ? 1 : 0;
+      const score2 = selectedPhotoIndex === 1 ? 1 : 0;
+
+      const newRating1 = photo1.rating + kFactor * (score1 - expectedScore1);
+      const newRating2 = photo2.rating + kFactor * (score2 - expectedScore2);
+
+      photo1.rating = newRating1;
+      photo2.rating = newRating2;
+
+      setIteration(iteration + 1);
+      setPhotos(updatedPhotos);
+      setNextPhotoIndices();
+    },
+    [
+      iteration,
+      photoIndex1,
+      photoIndex2,
+      photos,
+      setIteration,
+      setPhotos,
+      setNextPhotoIndices,
+    ]
+  );
 
   useEffect(() => {
     // Check if ranks have stabilized
@@ -129,14 +141,14 @@ const PhotoRankingApp: React.FC = () => {
             <img src={`photos/${photos[photoIndex1].url}`} alt="Photo 1" />
           </div>
 
-          <h2> {photos[photoIndex1].id}</h2>
+          <h2>{photos[photoIndex1].id}</h2>
           <button onClick={() => handlePhotoSelection(0)}>Choose</button>
         </div>
         <div>
           <div className="image-container">
             <img src={`photos/${photos[photoIndex2].url}`} alt="Photo 2" />
           </div>
-          <h2> {photos[photoIndex2].id}</h2>
+          <h2>{photos[photoIndex2].id}</h2>
           <button onClick={() => handlePhotoSelection(1)}>Choose</button>
         </div>
       </div>
